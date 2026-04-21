f73a1e214beeded28ba9f6a8959e509c44f9cc7f
Manojkumar-ANIMAN <animan31802@gmail.com>
2026-04-21 18:54:12 +0530

    TimeoutException

--- src/main/java/TestSelenium.java
+++ src/main/java/TestSelenium.java
@@ -1,6 +1,13 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverInfo;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class TestSelenium {
     public static void main(String[] args) {
@@ -13,11 +20,15 @@ public class TestSelenium {
 
         driver.findElement(By.name("login-button")).click();
 
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("app_logo")));
+            System.out.println("Element found → Login successful");
+        } catch (TimeoutException e) {
+            System.out.println("Element NOT found → Login failed or page not loaded");
         }
+
+        System.out.println("Login Successful!");
 
         driver.quit();
     }
