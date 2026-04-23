4175927c223173ad57df190af676cd03fffe7423
Manojkumar-ANIMAN <animan31802@gmail.com>
2026-04-23 18:15:09 +0530

    Test commit

--- src/test/java/TestSeleniumTest.java
+++ src/test/java/TestSeleniumTest.java
@@ -43,7 +43,6 @@ public class TestSeleniumTest {
 
     @Test
     public void loginStandardTest() {
-        // loginStandardTest
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.saucedemo.com/");
         var userNameField = driver.findElement(By.id("user-name"));
